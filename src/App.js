795ea8beb6c2c6e4c440dff27f434e361adff7bf
CarolinaTC <a.carolina.carrilho@gmail.com>
2021-05-18 18:14:20 +0100

    feat: add form and todo components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import React, {useState} from "react";
 import './App.css';
 
+// import components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+
+
+
 function App() {
+
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <h1>hello</h1>
+      <header>
+        <h1> To Do List  </h1>
+      </header>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList />
 
     </div>
   );
